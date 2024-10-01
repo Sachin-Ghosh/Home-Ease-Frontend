5b61e7ec6d624016ceccb15027a719ef7ab7fa5e
manas vikas gunjal <manasgunjal67@gmail.com>
2024-10-01 23:09:40 +0530

    vendorpage added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
+
 
 export default function Navbar() {
   return (
     <nav className="navbar bg-base-100 shadow-lg">
-      <div className="navbar-start">
-        <a className="btn btn-ghost normal-case text-xl">HomeEase</a>
+      <div className="navbar-start ">
+      <Link href="/" className="text-2xl font-bold text-blue-600 ml-5">
+            Home Ease
+       </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li><a>Home</a></li>
+          <li><a href='/homePage'>Home</a></li>
           <li><a>Services</a></li>
           <li><a>About Us</a></li>
           <li><a>Contact</a></li>
